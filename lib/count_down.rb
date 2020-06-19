646af91429a427c2cff68e859224f870e4b16eef
Arnie Serrano <arniekidd@gmail.com>
2020-06-19 20:29:44 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,9 +1,9 @@
 # Write your code here
-magic_exit_number=7
-count=0
+count=10
 
-while count<10 do
-  break if count==magic_exit_number
-  puts"I am the #{count}, I love to count!"
-  count=count+1
+while count>0 do
+  if count==10
+    puts "Happy New Year"
+  puts "#{count}"
+  count=count-1
 end
