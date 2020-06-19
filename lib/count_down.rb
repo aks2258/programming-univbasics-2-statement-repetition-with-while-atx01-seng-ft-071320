d93d2354713ebf6c42b19b94f3843f155db6031f
Arnie Serrano <arniekidd@gmail.com>
2020-06-19 20:31:22 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,9 +1,8 @@
 # Write your code here
 count=10
 
-while count>0 do
-  puts "#{count}"
-  count=count-1
-  if count==0
-    puts "Happy New Year!"
+while count>=0 do
+  puts count
+  count-=1
 end
+puts "Happy New Year!"
